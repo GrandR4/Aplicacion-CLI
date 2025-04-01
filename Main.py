414e220e5df407d2ec409d01a6fb2e2ac34b88e3
GrandR4 <rodriguezlima1984@gmail.com>
2025-04-01 12:01:49 -0400

    Usado Python en Visual Studio Code

--- Main.py
+++ Main.py
@@ -1,15 +1,15 @@
-# Aplicacion-CLI
-
 import os
 
-DIRECTORIO_ACTUAL = os.getcwd()
+DIRECTORIO_ACTUAL = os.getcwd() #Se usa las mayuscalas para indicar que es una constante
+                                #Tambien usamos getcwd() para que obtener el directeorio de trabajo actual
 
-def impresion(mensaje):
+def impresion(mensaje): #La funcion impresion la usamos para monstrar la direccion del directorio 
     print("="*70)
     print(mensaje)
     print("="*70)
 
-def lista_de_archivos(directorio):
+def lista_de_archivos(directorio):  #La funcion lista_de_archivos es usada para 
+                                    #Revisar los archivos en el directorio y posteriormente mostrarlo
     try:
         impresion(f"Contenido de La Carpeta: {directorio}" )
         archivos = os.listdir(directorio)
@@ -20,4 +20,4 @@ def lista_de_archivos(directorio):
     except PermissionError:
         print("No tienes permiso para acceder a este directorio.")
 
-lista_de_archivos(DIRECTORIO_ACTUAL)
+lista_de_archivos(DIRECTORIO_ACTUAL) #Llamamos a la funcion 
